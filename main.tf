2f480ad6ec61b28b66400fb93b625693cee44ee6
kennedy N <kenlabsolutions@yahoo.com>
2023-05-30 03:18:15 -0400

    Addes ingress

--- main.tf
+++ main.tf
@@ -5,9 +5,10 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0d659ab07f0f08020"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
     user_data = <<-EOF
                 #!/bin/bash
-                echo "Hello Ninjas"> index.html
+                echo "Hello Ninjas from space"> index.html
                 nohup busybox httpd -f -p 8080 &
                 EOF
     user_data_replace_on_change = true
@@ -15,5 +16,16 @@ resource "aws_instance" "example" {
     tags = {
       Name="terraform trials"
     }
+  
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
 }
