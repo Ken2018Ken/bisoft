6c50bcdc73c3d4de0ac42c4d5b5b5cd9a58563c0
kennedy N <kenlabsolutions@yahoo.com>
2023-05-30 02:26:32 -0400

    Modified Ami in main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-05171730429275e8e"
+    ami = "ami-0d659ab07f0f08020"
     instance_type = "t2.micro"
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello Ninjas"> index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    user_data_replace_on_change = true
 
     tags = {
       Name="terraform trials"
